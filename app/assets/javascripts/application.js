502b3cd1e1f1633a0ffd7b6a078b880c7284a8ab
Michael <mr.enviro@gmail.com>
2019-06-20 19:10:02 +0000

    Installed bootstrap and jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,20 @@
 //
 //= require rails-ujs
 //= require activestorage
+
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
+//= require popper
+//= require tether
+//= require bootstrap-sprockets
 //= require_tree .
+
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
+//= require jquery3
+//= require popper
+//= require bootstrap
